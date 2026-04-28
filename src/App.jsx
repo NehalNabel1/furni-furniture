56794febe266b11dab35ef553b5294046f67fc61
NehalNabel1 <nehalnabel6464@gmail.com>
2026-04-28 18:49:55 +0300

    last version

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,32 @@ import { Box } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme/theme";
 
-import Navbar           from "./components/Navbar";
-import HeroSection      from "./components/HeroSection";
-import ProductsSection  from "./components/ProductsSection";
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import ProductsSection from "./components/ProductsSection";
 import WhyChooseUsSection from "./components/WhyChooseUsSection";
-import InteriorSection  from "./components/InteriorSection";
-import BlogSection      from "./components/BlogSection";
-import Footer           from "./components/Footer";
+import InteriorSection from "./components/InteriorSection";
+import BlogSection from "./components/BlogSection";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Box sx={{ bgcolor: "background.default", minHeight: "100vh", width: "100%", overflowX: "hidden", margin: 0, padding: 0 }}>
+      <Box
+        sx={{
+          bgcolor: "background.default",
+          minHeight: "100vh",
+          width: "100%",
+          overflowX: "hidden",
+          margin: 0,
+          padding: 0,
+        }}
+      >
         <Navbar />
         <HeroSection />
         <ProductsSection />
-        <WhyChooseUsSection />
         <InteriorSection />
+        <WhyChooseUsSection />
         <BlogSection />
         <Footer />
       </Box>
